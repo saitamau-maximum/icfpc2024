8be6fa8f06d71535eda8ad3c3ccec1c436a6282a
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-06-29 02:23:15 +0900

    feat: add some utility scripts

---
+++ src/bin/encrypt.rs
@@ -0,0 +1,12 @@
+use icfpc2024::{tokenizer::Tokenizer, util::deconvert_string};
+use std::io::stdin;
+
+fn main() {
+    let text = {
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).unwrap();
+        buffer
+    };
+    let text = text.trim();
+    println!("ENCRYPTED: S{}", deconvert_string(text.to_string()));
+}
