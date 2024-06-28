c2a73ae617001ca9aa2a1e4129b8391ecf186c47
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-06-29 04:07:55 +0900

    feat: implement downloader

---
+++ src/bin/download.rs
@@ -0,0 +1,69 @@
+use dotenv::dotenv;
+use icfpc2024::icfp::{
+    evaluator::Evaluator,
+    parser::{Node, Parser},
+    tokenizer::Tokenizer,
+    util::{convert_string, deconvert_string},
+};
+use reqwest;
+use std::time;
+use std::{env, io::stdin};
+
+use serde::{Deserialize, Serialize};
+
+// =========== CONFIG ===========
+const API_URL: &str = "https://boundvariable.space/communicate";
+const COMMAND: &str = "get spaceship";
+const SAVE_DIR: &str = "downloads";
+const START_ID: usize = 22;
+const END_ID: usize = 25;
+// ==============================
+
+struct Env {
+    token: String,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let env = Env {
+        token: env::var("TOKEN").expect("TOKEN must be set"),
+    };
+
+    let client = reqwest::Client::new();
+
+    std::fs::create_dir_all(SAVE_DIR).unwrap();
+    for i in START_ID..=END_ID {
+        let text = deconvert_string(format!("{}{}", COMMAND, i));
+        let command = format!("S{}", text);
+
+        let res = client
+            .post(API_URL)
+            .header("Authorization", format!("Bearer {}", env.token))
+            .header("Content-Type", "text/plain")
+            .body(command)
+            .send()
+            .await;
+
+        match res {
+            Ok(res) => {
+                let body = res.text().await.unwrap();
+                let tokens = Tokenizer::new(&body).tokenize();
+                let node = Parser::new(&tokens).parse();
+                let result = Evaluator::new(node).evaluate();
+                let text = match result {
+                    Node::String(text) => text,
+                    _ => panic!("Result is not a string"),
+                };
+                std::fs::write(format!("{}/{}.txt", SAVE_DIR, i), text).unwrap();
+            }
+            Err(e) => {
+                eprintln!("Error: {:?}", e);
+            }
+        }
+
+        // 3秒待つ
+        tokio::time::sleep(time::Duration::from_secs(3)).await;
+    }
+}
