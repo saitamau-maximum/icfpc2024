04a09b7d58b0ff4cf1c6a6e759453a1d22dafd92
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-06-29 11:12:05 +0900

    feat: implement evaluator

---
+++ src/bin/eval.rs
@@ -0,0 +1,24 @@
+use icfpc2024::icfp::evaluator::Evaluator;
+use icfpc2024::icfp::parser::{Node, Parser};
+use icfpc2024::icfp::tokenizer::Tokenizer;
+use std::io::stdin;
+
+fn main() {
+    let text = {
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).unwrap();
+        buffer
+    };
+    let text = text.trim();
+    let mut tokenizer = Tokenizer::new(text);
+    let result = tokenizer.tokenize();
+    let mut parser = Parser::new(&result);
+    let result = parser.parse();
+    let evaluator = Evaluator::new(result);
+    let result = evaluator.evaluate();
+
+    match result {
+        Node::String(s) => println!("{}", s),
+        _ => panic!("Unexpected result: {:?}", result),
+    }
+}
